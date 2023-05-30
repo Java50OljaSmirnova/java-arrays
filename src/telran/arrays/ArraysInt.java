e76497c8ce399944c89d7c4003dc207746ba1b9e
oljas <olja.smirnov93@gmail.com>
2023-05-30 21:14:03 +0300

    CW #2 Arrays testing

---
+++ src/telran/arrays/ArraysInt.java
@@ -0,0 +1,26 @@
+package telran.arrays;
+
+import java.util.Arrays;
+
+public class ArraysInt {
+public static int[] addNumber(int [] array, int number) {
+	int[] res = Arrays.copyOf(array, array.length + 1);
+	res[array.length] = number;
+	return res;
+}
+public static int[] insertNumber(int[] array, int index, int number) {
+	int [] res = new int[array.length + 1];
+	System.arraycopy(array, 0, res, 0, index);
+	res[index] = number;
+	System.arraycopy(array, index, res, index + 1, array.length - index);
+	return res;
+}
+public static int[] removeNumber(int[] array, int index) {
+	//TODO
+	return null;
+}
+public static int[] insertNumberSorted(int[] arraySorted, int number) {
+	//TODO
+	return null;
+}
+}
